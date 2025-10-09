c123b9cffbdbf785251c46a01365e26a73645367
jacobiversen <s221341@dtu.dk>
2025-10-09 12:54:17 +0200

    arrival times løse

--- eksamenssæt/2023_12_exam_all_files/2023_12_exam/arrival_times.py
+++ eksamenssæt/2023_12_exam_all_files/2023_12_exam/arrival_times.py
@@ -1,7 +1,13 @@
 def arrival_times(schedule, delay):
-    arrival_string = [i.split(':') for i in schedule]
-    arrival_numbers = []
-    print(arrival_string)
+    result = []
+    for t in schedule:
+        h, m = map(int, t.split(":"))
+        total = h * 60 + m
+        total = (total + delay) % (24 * 60)     # håndterer evt. næste dag
+        hh = total // 60
+        mm = total % 60
+        result.append(f"{hh:02d}:{mm:02d}")
+    return result
 
 print(arrival_times(['12:37', '08:10'], 25))
 
